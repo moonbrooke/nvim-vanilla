9fe16bcc9f57eb2862822bec3fcce935f017f0cf
moonbrooke <smoeland@gmail.com>
2025-11-14 18:12:27 +0700

    options: change stuff

--- lua/options.lua
+++ lua/options.lua
@@ -1,8 +1,8 @@
 -- theme & transparency
 vim.cmd.colorscheme("unokai")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 
 -- Basic settings
 vim.opt.number = true                              -- Line numbers
