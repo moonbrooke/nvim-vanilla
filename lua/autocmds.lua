8b0534a60bd1cfbfa89994f13a1a6e58c4a049d3
moonbrooke <smoeland@gmail.com>
2025-11-15 06:12:38 +0700

    autocmds: add format on save, remove terminal stuff

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,4 +1,8 @@
--- Basic autocommands
+-- Format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+    callback = function() vim.lsp.buf.format() end,
+})
+
 local augroup = vim.api.nvim_create_augroup("UserConfig", {})
 
 -- Highlight yanked text
@@ -40,26 +44,6 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
--- Auto-close terminal when process exits
-vim.api.nvim_create_autocmd("TermClose", {
-    group = augroup,
-    callback = function()
-        if vim.v.event.status == 0 then
-            vim.api.nvim_buf_delete(0, {})
-        end
-    end,
-})
-
--- Disable line numbers in terminal
-vim.api.nvim_create_autocmd("TermOpen", {
-    group = augroup,
-    callback = function()
-        vim.opt_local.number = false
-        vim.opt_local.relativenumber = false
-        vim.opt_local.signcolumn = "no"
-    end,
-})
-
 -- Auto-resize splits when window is resized
 vim.api.nvim_create_autocmd("VimResized", {
     group = augroup,
@@ -78,4 +62,3 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         end
     end,
 })
-
