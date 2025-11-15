e45c78ef5292e4c4ed1832cd7ba80915b0a0e8a2
moonbrooke <smoeland@gmail.com>
2025-11-15 21:44:55 +0700

    autopair: edit

--- lua/autopair.lua
+++ lua/autopair.lua
@@ -1,7 +1,8 @@
+-- Autopair
 vim.keymap.set("i", "(", "()<Left>", { noremap = true, silent = true })
 vim.keymap.set("i", "[", "[]<Left>", { noremap = true, silent = true })
 vim.keymap.set("i", "{", "{}<Left>", { noremap = true, silent = true })
 
 -- Quotes
-vim.keymap.set("i", "\"", "\"\"<Left>", { noremap = true })
-vim.keymap.set("i", "'", "''<Left>", { noremap = true })
+vim.keymap.set("i", "\"", "\"\"<Left>", { noremap = true, silent = true })
+vim.keymap.set("i", "'", "''<Left>", { noremap = true, silent = true })
