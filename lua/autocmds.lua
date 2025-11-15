b7a69b7b8dcea4756464327b0cc75e64634faa66
moonbrooke <smoeland@gmail.com>
2025-11-15 21:45:31 +0700

    autocmds: auto trigger completion

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,9 +1,19 @@
+local augroup = vim.api.nvim_create_augroup("UserConfig", {})
+
 -- Format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
     callback = function() vim.lsp.buf.format() end,
 })
 
-local augroup = vim.api.nvim_create_augroup("UserConfig", {})
+-- Autocompletion
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client ~= nil and client:supports_method("textDocument/completion") then
+            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+        end
+    end,
+})
 
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
