9eaf1f5450b8f4c405057f702f09cc92541164dc
moonbrooke <smoeland@gmail.com>
2026-04-30 17:35:05 +0700

    4 spaces indent

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -49,8 +49,8 @@ vim.api.nvim_create_autocmd("FileType", {
     group = augroup,
     pattern = { "javascript", "typescript", "json", "html", "css" },
     callback = function()
-        vim.opt_local.tabstop = 2
-        vim.opt_local.shiftwidth = 2
+        vim.opt_local.tabstop = 4
+        vim.opt_local.shiftwidth = 4
     end,
 })
 
