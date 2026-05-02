4753b3a97c77386443aa249148718f10eb77e7b3
moonbrooke <smoeland@gmail.com>
2026-05-02 19:20:26 +0700

    autopair smart enter

--- lua/autopair.lua
+++ lua/autopair.lua
@@ -3,6 +3,21 @@ vim.keymap.set("i", "(", "()<Left>", { noremap = true, silent = true })
 vim.keymap.set("i", "[", "[]<Left>", { noremap = true, silent = true })
 vim.keymap.set("i", "{", "{}<Left>", { noremap = true, silent = true })
 
+-- Smart enter
+vim.keymap.set("i", "<CR>", function()
+    local line = vim.fn.getline(".")
+    local col = vim.fn.col(".")
+    local prev_char = string.sub(line, col - 1, col - 1)
+    local next_char = string.sub(line, col, col)
+
+    if (prev_char == "{" and next_char == "}") or
+        (prev_char == "[" and next_char == "]") or
+        (prev_char == "(" and next_char == ")") then
+        return "<CR><Esc>O"
+    end
+    return "<CR>"
+end, { expr = true, noremap = true })
+
 -- Quotes
 vim.keymap.set("i", "\"", "\"\"<Left>", { noremap = true, silent = true })
 vim.keymap.set("i", "'", "''<Left>", { noremap = true, silent = true })
